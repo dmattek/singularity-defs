ee402b8d9cea17b210e175581919a228dc67a16a
Maciej Dobrzynski <macdobryd@gmail.com>
2019-02-13 13:19:52 +0100

    Bug fixes

--- R/runr3.2.3.sh
+++ R/runr3.2.3.sh
@@ -3,10 +3,10 @@
 # Singularity wrapper script
 
 # Location of Singularity image
-IMLOC=/opt/local/R/rubuntu.img
+IMLOC=/opt/local/R/rubuntu.3.2.3.img
 
 # List of directories to bind with the image
 BNDDIR="-B /scratch -B /mnt/imaging.data -B /opt/local"
 
 # execute "runscript" section of the image
-singularity exec $BNDDIR $IMLOC Rscript $@
+singularity exec $BNDDIR $IMLOC R $@
---
+++ ShuttleTracker/st-tiff_expander.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Singularity wrapper script
+
+# Location of Singularity image
+IMLOC=/opt/local/ShuttleTracker/st.simg
+
+# List of directories to bind with the image
+BNDDIR="-B /scratch -B /mnt/imaging.data"
+
+# execute "runscript" section of the image
+singularity run --app tiff_expander $BNDDIR $IMLOC $@
