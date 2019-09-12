9340289ff41193eda59516cfe94b3a0ab98b99de
Maciej Dobrzynski <macdobry@gmail.com>
2019-09-12 13:16:28 +0000

    Added R 3.6.1

---
+++ R/runr3.6.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Singularity wrapper script
+
+# Location of Singularity image
+IMLOC=/opt/local/R/rstudio.3.6.1.simg
+
+# List of directories to bind with the image
+BNDDIR="-B /scratch -B /mnt/imaging.data -B /opt/local"
+
+# execute "runscript" section of the image
+singularity run --app R $BNDDIR $IMLOC $@
