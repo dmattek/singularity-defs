7610324edb003f143adecb66acf4d251130d3df1
MD <md@server.net>
2022-10-13 15:35:31 +0200

    update

---
+++ cellprofiler/runcp424.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Singularity wrapper script
+
+# Location of Singularity image
+IMLOC=/opt/local/cellprofiler/cp4.2.4-ubuntu20.04.simg
+
+# List of directories to bind with the image
+BNDDIR="-B /run -B /scratch -B /mnt/imaging.data -B /mnt/external.data"
+
+# execute "runscript" section of the image
+slurm-or-interactive singularity run $BNDDIR $IMLOC $@
