2c930baf543cd392ab0cd8e9c221280c7383f726
macdobry <macdobry@gmail.com>
2022-10-19 15:01:48 +0200

    update

---
+++ R/runrscript4.2.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Singularity wrapper script
+
+# Location of Singularity image
+IMLOC=/opt/local/R/r.4.2.1.sif
+
+# List of directories to bind with the image
+BNDDIR="-B /scratch -B /mnt/imaging.data -B /mnt/external.data -B /opt/local"
+
+# execute "runscript" section of the image
+slurm-or-interactive singularity run --app Rscript $BNDDIR $IMLOC $@
