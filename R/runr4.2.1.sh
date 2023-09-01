650fee7139fdec85ca3b1feb427d6179a3212132
macdobry <macdobry@gmail.com>
2023-09-01 20:14:18 +0200

    update

--- R/runr4.2.1.sh
+++ R/runr4.2.1.sh
@@ -6,7 +6,7 @@
 IMLOC=/opt/local/R/r.4.2.1.sif
 
 # List of directories to bind with the image
-BNDDIR="-B /scratch -B /mnt/imaging.data -B /mnt/external.data -B /opt/local"
+BNDDIR="-B /scratch -B /mnt/imaging.data -B /mnt/external.data -B /mnt/towbin.data -B /mnt/heussler.data -B /opt/local"
 
 # execute "runscript" section of the image
 slurm-or-interactive singularity run --app R $BNDDIR $IMLOC $@
--- cellprofiler/runcp421.sh
+++ cellprofiler/runcp421.sh
@@ -3,7 +3,7 @@
 # Singularity wrapper script
 
 # Location of Singularity image
-IMLOC=/opt/local/cellprofiler/cp3ubuntu.img
+IMLOC=/opt/local/cellprofiler/cp4.2.1-ubuntu20.04.simg
 
 # List of directories to bind with the image
 BNDDIR="-B /run -B /scratch -B /mnt/imaging.data -B /mnt/external.data -B /mnt/towbin.data -B /mnt/heussler.data"
